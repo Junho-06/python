982eb90081c819cd9647aef13363e5532f553d1a
박준호 <103944584+Junho-06@users.noreply.github.com>
2025-02-21 20:53:36 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/healthcheck')
+def healthcheck():
+    return jsonify({"status": "OK"}), 200
+
+@app.route('/v1/app')
+def version():
+    return jsonify({"body": "This is App v1"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
