89513af485f2593e367f138f3ba13e9f8619ed04
박준호 <103944584+Junho-06@users.noreply.github.com>
2025-02-22 11:24:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def healthcheck():
     return jsonify({"status": "OK"}), 200
 
-@app.route('/v1/app')
+@app.route('/v2/app')
 def version():
-    return jsonify({"body": "This is App v1"}), 200
+    return jsonify({"body": "This is App v2"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
